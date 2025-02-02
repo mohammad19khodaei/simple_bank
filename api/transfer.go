546a0c5b5a730ad674ba61dc849e3161bb7b96f4
Mohammad Khodaei <mohammad.khodaei@arvancloud.ir>
2025-02-03 00:33:51 +0330

    test(transfer): Refactoring transfer API tests

---
+++ api/transfer.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	db "github.com/mohammad19khodaei/simple_bank/db/sqlc"
+)
+
+type transferRequest struct {
+	FromAccountID int32 `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int32 `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64 `json:"amount" binding:"required,gt=0"`
+}
+
+func (s *server) transferHandler(ctx *gin.Context) {
+	var request transferRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, s.errorResponse(err))
+		return
+	}
+
+	fromAccount, err := s.store.GetAccount(ctx, request.FromAccountID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, s.errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, s.errorResponse(err))
+		return
+	}
+
+	if fromAccount.Balance < request.Amount {
+		ctx.JSON(http.StatusPaymentRequired, s.errorResponse(fmt.Errorf("insufficient balance")))
+		return
+	}
+
+	toAccount, err := s.store.GetAccount(ctx, request.ToAccountID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, s.errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, s.errorResponse(err))
+		return
+	}
+
+	if fromAccount.Currency != toAccount.Currency {
+		ctx.JSON(http.StatusBadRequest, s.errorResponse(fmt.Errorf("from account currency %s mismatch to account currency %s", fromAccount.Currency, toAccount.Currency)))
+		return
+	}
+
+	transfer, err := s.store.TransferTx(ctx, db.TransferTxParams{
+		FromAccountID: fromAccount.ID,
+		ToAccountID:   toAccount.ID,
+		Amount:        request.Amount,
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, s.errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, transfer)
+}
